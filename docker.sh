2d4202a97dab325f3c861c853f09c3162d6d83c8
wilsoncwip <wilsoncwip@hotmail.com>
2018-09-14 16:03:54 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,2 +1,2 @@
 sudo yum install -y yum-utils device-mapper-persistent-data lvm2
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum-config-manager --add-repo "https://download.docker.com/linux/centos/docker-ce.repo"
